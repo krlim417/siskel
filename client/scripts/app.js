c2026f966b05f9d98db12d4fdad9cd26641fe502
krlim417 <krlim417@gmail.com>
2018-04-17 23:34:36 -0700

    Re-render when the collection sorts

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -80,7 +80,7 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.collection.on('sort', this.render, this);
   },
 
   render: function() {
